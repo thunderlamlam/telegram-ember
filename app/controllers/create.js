bdefa8106fba4a90d26af6db25112fec47ab5d7e
thunderlamlam <lam.h.emily@gmail.com>
2014-05-25 17:58:56 -0700

    added error handling for bad login

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -1,20 +1,27 @@
 export default Ember.ObjectController.extend({
+  loginFailed: false,
   actions: {
     login: function() {
       //get the username and password from the template
       var username = this.get('newUsername');
       var password = this.get('newPassword');
-      
       console.log(username);
       console.log(password);
-      console.log(this.store.find('User', 1));
-      if(username == 'model.username' && password == 'model.password'){
-        console.log('yay');
-
+      if(username == 'emily' && password == '1234'){
+        //success
+        console.log('success');
+        this.setProperties({
+          loginFailed:false
+        });
       }
       else{
-        console.log("yikes");}
+        //failure
+        this.setProperties({
+          loginFailed:true
+        });
       }
+      
     }
+  }
   
 });
