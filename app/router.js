c09a210e30545aed6bd683d27bdd92bdc59b70ce
thunderlamlam <lam.h.emily@gmail.com>
2014-05-27 18:26:36 -0700

    change router file

--- app/router.js
+++ app/router.js
@@ -3,16 +3,15 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-	this.resource('create', { path: '/'}, function () {
-		//additional child routes
-		this.route('recover_pw');
-		this.route('reset_pw');
-		this.resource('login', {path: '/:user_id' }, function(){
-			this.route('post');
-		});
+  this.resource('create', { path: '/'}, function () {
+  //additional child routes
+    this.route('recover_pw');
+    this.route('reset_pw');
+    this.resource('login', {path: '/:user_id' }, function(){
+	  this.route('post');
+	  });
 		
-	}
-	);
+    });
 });
 
 export default Router;
