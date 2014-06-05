522fcf37d90977db065cbb75541286de1542d366
thunderlamlam <lam.h.emily@gmail.com>
2014-06-05 14:19:14 -0700

    just so that Emily user works need to figure out the hbs part

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,7 +1,10 @@
 export default Ember.Route.extend({
   model: function(params, transition) {
-    return { user: params.user};
+    return { user: params.user_id };
   },
+  serialize: function(model) {
+    return { user: model.get('user_id') };
+  } ,
   renderTemplate: function(controller) {
     this.render('dashboard');
     this.render('topbar', {into: 'dashboard', outlet: 'topbar'});
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,9 @@
 export default Ember.Route.extend({
 
   model: function(params, transition) {
-    return { user: params.user };
+    return { user: params.user_id };
   },
   serialize: function(model) {
-    return { user: model.get('user') };
+    return { user: model.get('user_id') };
   } 
 });
