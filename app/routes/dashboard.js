89d815764e90f00ca53062c40c3092361c0698ed
thunderlamlam <lam.h.emily@gmail.com>
2014-06-05 10:13:38 -0700

    updating a bit more

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,9 +1,6 @@
 export default Ember.Route.extend({
   model: function(params, transition) {
-    return { user: params.user_id };
-  },
-  serialize: function(model) {
-    return { user: model.get('user_id') };
+    return { user: params.user};
   },
   renderTemplate: function(controller) {
     this.render('dashboard');
