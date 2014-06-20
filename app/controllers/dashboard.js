1b7ab877faebea5beb245a7834a89b5b2617434e
thunderlamlam <lam.h.emily@gmail.com>
2014-06-20 12:03:42 +0800

    change some of the session injection

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,5 +1,8 @@
 export default Ember.ObjectController.extend({
-  
+  uppercaseName: function() {
+    console.log('The session object is accessible from any controller:', this.get('session.user.name'), this.get('session.isAuthenticated'));
+    return this.get('model').get('name').toUpperCase();
+  }.property('model.name')
   //var promise = this.store.find('user');
   //promise.then(function(userobject){
         //console.log(userobject);
