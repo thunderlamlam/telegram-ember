57acc8dd6e25e64aa40b1e91cf19b7f3fcab9486
thunderlamlam <lam.h.emily@gmail.com>
2014-06-23 11:54:42 -0700

    update session

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,9 +1,12 @@
 export default Ember.Route.extend({
   
-  model: function(params, transition) {
-    return { user: params.user_id };
-  },
-  serialize: function(model) {
-    return { user: model.get('user_id') };
+  //model: function(params, transition) {
+  //  return { user: params.user_id };
+  //},
+  //serialize: function(model) {
+  //  return { user: model.get('user_id') };
+  //}
+  model: function () {
+    return this.get('session').get('user');
   }
 });
