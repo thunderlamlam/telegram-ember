e47ab4ad75205fd9452eae3a80f3fc13906acf66
thunderlamlam <lam.h.emily@gmail.com>
2014-06-24 10:15:28 -0700

    get rid of unnecessary model info in user.js

--- app/routes/user.js
+++ app/routes/user.js
@@ -6,7 +6,10 @@ export default Ember.Route.extend({
   //serialize: function(model) {
   //  return { user: model.get('user_id') };
   //}
-  model: function () {
-    return this.get('session').get('user');
-  }
+  //model: function () {
+  //  return this.get('session').get('user');
+  //}
+  //model: function () {
+  //  return this.modelFor('post').get('post_content');
+  //},
 });
