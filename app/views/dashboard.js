d1c04f529ec08eac945a7909b3374096b8864390
thunderlamlam <lam.h.emily@gmail.com>
2014-06-24 14:20:42 -0700

    create view for dashboard template so it has a new background

---
+++ app/views/dashboard.js
@@ -0,0 +1,9 @@
+export default Ember.View.extend({
+  templateName: 'dashboard',
+  didInsertElement: function() {
+    $('body').addClass('dashboard')
+  },
+  willClearRender: function() {
+    $('body').removeClass('dashboard')
+  }
+});
