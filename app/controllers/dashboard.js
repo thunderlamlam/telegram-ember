6032c53cc61e497482fc41a0b61d0d6843da2b07
thunderlamlam <lam.h.emily@gmail.com>
2014-06-30 00:47:41 -0700

    update dashboard page with text count

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,8 +1,29 @@
 export default Ember.ObjectController.extend({
+  isEditing: false,
   uppercaseName: function() {
     console.log('The session object is accessible from any controller:', this.get('session.user.name'), this.get('session.isAuthenticated'));
     return this.get('model').get('name').toUpperCase();
-  }.property('model.name')
+  }.property('model.name'),
+
+  actions: {
+  	edit: function() {
+  	  var controller = this;
+  	  controller.set('isEditing', true);
+  	  },
+  	doneEditing: function() {
+  	  controller.set('isEditing', false);
+  	  }
+  },
+
+  txtCount: function() {
+    return this.get('postBody.length');
+  }.property('postBody')
+
+  ,
+
+  characters: function() {
+    return this.get('postBody.length') === 1 ? 'character' : 'characters';
+  }.property('postBody')
   //var promise = this.store.find('user');
   //promise.then(function(userobject){
         //console.log(userobject);
