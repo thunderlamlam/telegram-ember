959b8985bdc08fe9eb2a3b97fc68d72430d4fb9a
thunderlamlam <lam.h.emily@gmail.com>
2014-07-02 12:59:57 -0700

    create the create.js controller

---
+++ app/controllers/public/create.js
@@ -0,0 +1,12 @@
+export default Ember.ObjectController.extend({
+	actions: {
+    create: function() {
+      var controller = this;
+      //get the username and password from the template
+      var name = this.get('newName');
+      var username = this.get('newUsername');
+      var password = this.get('newPassword');
+      controller.transitionToRoute('/login');
+    }
+    }
+});
