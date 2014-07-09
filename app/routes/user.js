dcf00243fbded681888a1b0478a4b8990d6db7e5
thunderlamlam <lam.h.emily@gmail.com>
2014-07-08 22:30:28 -0400

    creating the index for user so post stuff appears like Andrei taught me the other day

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,19 +1,3 @@
 export default Ember.Route.extend({
-  
-  //model: function(params, transition) {
-  //  return { user: params.user_id };
-  //},
-  //serialize: function(model) {
-  //  return { user: model.get('user_id') };
-  //}
-  //model: function () {
-  //  return this.get('session').get('user');
-  //}
-  //model: function () {
-  //  return this.modelFor('post').get('post_content');
-  //},
 
-  //model: function() {
-  //  return this.store.find('post');
-  //}
 });
