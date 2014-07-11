8f3664b71185468a95f9faa778ee813eb704ea2b
thunderlamlam <lam.h.emily@gmail.com>
2014-07-10 17:35:11 -0700

    updating sign up flow

--- app/controllers/public/create.js
+++ app/controllers/public/create.js
@@ -1,12 +1,28 @@
 export default Ember.ObjectController.extend({
+  UserExisted: false,
 	actions: {
     create: function() {
       var controller = this;
-      //get the username and password from the template
-      var name = this.get('newName');
+      var dname = this.get('newName');
       var username = this.get('newUsername');
-      var password = this.get('newPassword');
-      controller.transitionToRoute('dashboard');
+      var dpassword = this.get('newPassword');
+
+      if(this.store.find('user', username)){
+        controller.setProperties({
+          UserExisted:true
+        });
+      }
+      else{
+        controller.setProperties({
+          UserExisted:false
+        });
+        var person = this.store.createRecord('user', {
+          id: username
+        });
+        controller.get('session').set('user', person);
+        controller.transitionToRoute('dashboard');
+
+      }        
     }
     }
 });
