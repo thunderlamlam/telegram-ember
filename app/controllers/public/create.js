64f71ebff4f62e555ad804094ffaf63a89beea73
thunderlamlam <lam.h.emily@gmail.com>
2014-07-10 17:57:45 -0700

    Updates on Thursday at 6pm

--- app/controllers/public/create.js
+++ app/controllers/public/create.js
@@ -7,22 +7,29 @@ export default Ember.ObjectController.extend({
       var username = this.get('newUsername');
       var dpassword = this.get('newPassword');
 
-      if(this.store.find('user', username)){
-        controller.setProperties({
-          UserExisted:true
-        });
-      }
-      else{
-        controller.setProperties({
-          UserExisted:false
-        });
-        var person = this.store.createRecord('user', {
-          id: username
-        });
-        controller.get('session').set('user', person);
-        controller.transitionToRoute('dashboard');
+      //if(this.store.find('user', username)){
+      //  controller.setProperties({
+      //    UserExisted:true
+      //  });
+      //}
+      //else{
+      //  controller.setProperties({
+      //    UserExisted:false
+      //  });
+        
+        //debugger;
 
-      }        
+        var person = this.store.createRecord('user', {
+          //id: username,
+          name: dname,
+          password: dpassword
+        });
+
+        person.save().then(function(userobject){
+          controller.get('session').set('user', userobject);
+          controller.transitionToRoute('dashboard');
+        });
+      //}        
     }
-    }
+  }
 });
--- app/models/user.js
+++ app/models/user.js
@@ -3,10 +3,7 @@ var User = DS.Model.extend({
   name: DS.attr('string'),
   profileImage: DS.attr('string'),
   posts: DS.hasMany('Post'),
-  //followers: DS.attr('string'),
   followers: DS.hasMany('user'),
-  //posts: DS.hasMany('post'),
-  //following: DS.attr('string')
   following: DS.hasMany('user')
   //user is put there because you are inputting the user object
 
@@ -22,7 +19,6 @@ User.reopenClass({
     profileImage: 'profile1.jpg',
     followers: ['maggie', 'cece'],
     //has many needs to take an array
-    //posts: '1',
     following: ['cece']
   },
   {
@@ -32,7 +28,6 @@ User.reopenClass({
     password: '1234',
     profileImage: 'profile2.jpg',
     followers: ['emily'],
-    //posts: '1',
     following: ['cece']
   },
   {
@@ -41,7 +36,7 @@ User.reopenClass({
     password: '1234',
     profileImage: 'profile3.jpg',
     followers: ['maggie'],
-    //posts: '1',
+
     following: ['emily']
   }
 
