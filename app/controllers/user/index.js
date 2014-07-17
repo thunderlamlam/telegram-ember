6358a7792b8ce4c99a17203f5a293f19661d0435
thunderlamlam <lam.h.emily@gmail.com>
2014-07-16 23:39:44 -0700

    give profile sorted and also remove button

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -1,3 +1,11 @@
 export default Ember.ArrayController.extend({
+  sortProperties: ['date'],
+  sortAscending: false,
+
+  actions: {remove: function(rpost) {
+      rpost.deleteRecord();
+      rpost.save();
+      }
+  }
 
 });
