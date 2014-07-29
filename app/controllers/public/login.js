66d1e83b25f823937fa10b93b9e4aecd8bb1ec23
thunderlamlam <lam.h.emily@gmail.com>
2014-07-29 06:02:50 -0700

    update on login.js so it works on backend instead of frontend

--- app/controllers/public/login.js
+++ app/controllers/public/login.js
@@ -12,25 +12,28 @@ export default Ember.ObjectController.extend({
       console.log(username);
       //console.log(password);
       //userobject is returned by my query
-      var promise = this.store.find('user', username);
-      promise.then(function(userobject){
+      var promise = this.store.find('user', {id: username, password: password, operation: 'login'});      
+      promise.then(function(response){
+        var loginUser = response.get('firstObject');
+        controller.get('session').set('user', loginUser);
+        controller.transitionToRoute('dashboard');
         //console.log(userobject);
         //check the password since the username is already checked
-        if(password === userobject.get('password')){
+        //if(password === userobject.get('password')){
 
-          controller.setProperties({
-          loginFailed:false
-        });
-          controller.get('session').set('user', userobject);
+        //  controller.setProperties({
+        //  loginFailed:false
+        //});
+          
       //trying to send it to /login page
-        controller.transitionToRoute('dashboard');
-      }
-      else{
+        
+      //}
+      //else{
       //failure
-        controller.setProperties({
-        loginFailed:true
-        });
-      }        
+      //  controller.setProperties({
+      //  loginFailed:true
+      //  });
+      //}        
         });
       
       
