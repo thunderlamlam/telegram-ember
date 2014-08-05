903f499863747b3964b09a70f2a23b3a45a23340
thunderlamlam <lam.h.emily@gmail.com>
2014-08-05 13:40:52 -0700

    application route

---
+++ app/routes/application.js
@@ -0,0 +1,17 @@
+export default Ember.Route.extend({
+
+  beforeModel: function() {
+
+   var route = this;
+    var promise = this.store.find('user', {authenticated: true});
+    return promise.then(function(users) {
+      if (users && users.get('length') > 0) {
+        var user = users.get('firstObject');
+        route.set('session.user', user);
+      }
+      return users;
+    });
+
+  }
+
+});
