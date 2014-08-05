8612f4ecc03e3e6df235ffda5651b4204f8f7479
thunderlamlam <lam.h.emily@gmail.com>
2014-08-05 00:13:32 -0700

    persistent session

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -5,9 +5,25 @@ export default Ember.Route.extend({
   //serialize: function(model) {
   //  return { user: model.get('user_id') };
   //} ,
+
+  beforeModel: function() {
+
+    var route = this;
+    var promise = this.store.find('user', {isAuthenticated: true});
+    return promise.then(function(users) {
+      if (users && users.get('length') > 0) {
+        var user = users.get('firstObject');
+        route.set('session.user', user);
+      }
+      return users;
+    });
+
+  },
+
   model: function () {
     //return this.get('session').get('user'); you don't need this anymore because every routes will have access to this session object and controller as well. the session object comes from the controller
     return this.store.find('Post');
+    
 
   }, 
 
--- app/routes/public.js
+++ app/routes/public.js
@@ -1,2 +1,3 @@
 export default Ember.Route.extend({
+
 });
