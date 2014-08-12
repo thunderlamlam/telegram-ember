f8e157433d3bafbc514b4b0612accf45efbcea1e
thunderlamlam <lam.h.emily@gmail.com>
2014-08-12 16:47:16 -0700

    get rid of post in user model

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,6 @@ var User = DS.Model.extend({
   password: DS.attr('string'),
   name: DS.attr('string'),
   profileImage: DS.attr('string'),
-  posts: DS.hasMany('Post'),
   followers: DS.hasMany('User'),
   following: DS.hasMany('User')
   //user is put there because you are inputting the user object
